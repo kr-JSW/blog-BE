d3c8989b809262b4956129ae3292f72173c08a93
정승원 <djdurk0817@gmail.com>
2023-11-13 22:04:29 +0900

    카테고리 이미지 삭제 추가

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,5 +1,11 @@
 const categoryModel = require('../model/categories');
 const postModel = require('../model/post');
+const fs = require('fs');
+
+function checkUrl(photoName) {
+    let expUrl = /^http[s]?:\/\/([\S]{3,})/i;
+    return expUrl.test(photoName);
+}
 
 const createCategory = (req, res) => {
     const { name, photo } = req.body;
@@ -24,10 +30,29 @@ const getCategory = async (req, res) => {
     return res.status(200).json({ answer: arr });
 };
 
-const delCategory = (req, res) => {
+const delCategory = async (req, res) => {
     const { value } = req.body;
-    categoryModel.findOneAndDelete({ name: value }).exec();
-    postModel.deleteMany({ categories: value }).exec();
+    let delFile = await categoryModel.findOneAndDelete({ name: value }).exec();
+    let manyDelFile = await postModel.find({ categories: value }).exec();
+    await postModel.deleteMany({ categories: value }).exec();
+
+    delFile = checkUrl(delFile.photo) ? null : delFile;
+    console.log('manyDelFile : ', manyDelFile);
+    try {
+        fs.unlink(`./images/${delFile.photo}`, (err) => {
+            console.log('카테고리 이미지 삭제 err : ', err);
+        });
+    } catch {}
+    manyDelFile.map((i) => {
+        const { photo } = i;
+        if (!checkUrl(photo)) {
+            try {
+                fs.unlink(`./images/${photo}`, (err) => {
+                    console.log('게시글 이미지 삭제 err : ', err);
+                });
+            } catch {}
+        }
+    });
 
     return res.status(200).json({ answer: '카테고리가 삭제되었습니다.' });
 };
